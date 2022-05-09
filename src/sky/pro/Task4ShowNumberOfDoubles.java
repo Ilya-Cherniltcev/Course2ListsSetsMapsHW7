0c328e9bdb3864c647e6a41f9bdac05dfd456b5c
Ilya Cherniltsev <cherniltcev@mail.ru>
2022-05-09 14:04:30 +0500

    ready

---
+++ src/sky/pro/Task4ShowNumberOfDoubles.java
@@ -0,0 +1,53 @@
+package sky.pro;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Task4ShowNumberOfDoubles {
+    public void printNumberOfDoubles(List<String> words) {
+        List<String> fraze = new ArrayList<String>();
+// ---------- делаем новый список с уникальными словами ----------
+        Task3UniqWords f = new Task3UniqWords();
+        fraze = f.findUniqWords(words);
+// ---------- сравниваем длину первоначального списка с новым -----
+        int lengthOfLists = words.size() - fraze.size();
+        if (lengthOfLists == 0) {
+            System.out.println("Повторяющихся слов нет");
+            return;
+        }
+// ---------- преобразуем полученный список в map, где ключ - строка,
+// ---------- а значение - число повторений данной строки -----
+        Map<String, Integer> frazeMap = new HashMap<>();
+        for (String s : fraze) {
+// --------   первоначально число повторений каждой строки равно 0
+            frazeMap.put(s, 0);
+        }
+        boolean isExist;
+        int numberOfDoubles = 0;
+        String key;
+// ------------ подсчитываем число повторений каждой строки ------------
+        for (int i = 0; i < words.size(); i++) {
+            key = words.get(i);
+            isExist = frazeMap.containsKey(key);
+            numberOfDoubles = frazeMap.get(key);
+            if (isExist) {
+                numberOfDoubles++;
+                frazeMap.replace(key, numberOfDoubles);
+            }
+        }
+        System.out.println(frazeMap);
+// ----------- подсчитываем число дублей, последовательно складывая все повторения (значения)
+        int value = 0;
+        numberOfDoubles = 0;
+        for (Map.Entry<String, Integer> entry : frazeMap.entrySet()) {
+            value = entry.getValue();
+            if (value != 1) {
+                numberOfDoubles += value;
+            }
+        }
+        System.out.println("Число дублей - " + numberOfDoubles);
+    }
+}
+
